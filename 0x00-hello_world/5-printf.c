2963c6f6763bcc1b7bedc20ec4a959955ac836c5
mayowa <mayowapopoola065@gmail.com>
2022-12-11 00:53:39 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main-Print a given phrase
+ * Return:0 if existed properly, non-zero otherwise
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return (0);
+}
