6381f297caa2629f4858e350d6b93440b3f79b45
mayowa <mayowapopoola065@gmail.com>
2022-12-23 08:43:50 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - print a gven phrase
  * Return: 0 if existed properly; non-zero otherwise
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n"); 
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
