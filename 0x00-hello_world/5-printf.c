537c6faa5be83c430251c4c64137daab9772e36b
mayowa <mayowapopoola065@gmail.com>
2022-12-23 08:48:03 -0800

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * but the outcome is a piece of art,
  * Return:0 if existed properly, non-zero otherwise
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
