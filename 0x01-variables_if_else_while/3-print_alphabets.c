bceed3d2d418913f878fec06367dddc6f4713d36
mayowa <mayowapopoola065@gmail.com>
2022-12-30 03:54:24 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets in lower cases and upp
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a', ch < = 'z', ch++)
+		putchar(ch);
+
+	for (ch = 'A', ch < = 'Z', ch++)
+		putchar(ch);
+
+	putchar('\n');
+	return (0);
+}
