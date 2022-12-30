aec2fdb64b9e88107ef19b755e31aa5af771cbad
mayowa <mayowapopoola065@gmail.com>
2022-12-30 09:23:04 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the lowercase alphabet in reverse 
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
